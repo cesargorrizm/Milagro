4813702bd4cc5d6248dff3336cb91ddd542ecafd
danielmartin2 <danielmartinteruel@gmail.com>
2022-05-05 14:36:09 +0200

    relacion tabla instancias y tipo y capacidades

---
+++ app/Models/Instancia.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Instancia extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'titulo', 'descripcion', 'tipo_id','capacidad_id'
+    ];
+}
---
+++ database/migrations/2022_05_05_140346_create_instancias_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstanciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instancias', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->longText('descripcion');
+            $table->unsignedBigInteger('tipo_id');
+            $table->unsignedBigInteger('capacidad_id')->nullable();
+            $table->foreign('tipo_id')->references('id')->on('tipos');
+            $table->foreign('capacidad_id')->references('id')->on('capacidads');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instancias');
+    }
+}
