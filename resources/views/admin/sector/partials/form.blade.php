1ec3db17d462241b666557df4251aca3c141c52b
cesargorrizm <cgorrim@iesch.org>
2022-05-09 10:28:01 +0200

    aas

--- resources/views/admin/sector/partials/form.blade.php
+++ resources/views/admin/sector/partials/form.blade.php
@@ -8,6 +8,7 @@
 ?>
 
 <div class="row">
+    <div class="column col-lg-6 form-group">
     <div class="col-lg-6 form-group">
         {!! Form::label('titulo', 'Titulo', ['class' => 'is-required']) !!}
         {!! Form::text('titulo', null, ['class' => 'form-control', 'placeholder' => 'Introduzca el Titulo', 'autocomplete' => 'off', 'required']) !!}
@@ -19,6 +20,18 @@
 
     </div>
     <div class="col-lg-6 form-group">
+        {!! Form::label('tipo_id', 'Tipo', ['class' => 'is-required']) !!}
+        {!! Form::select('role', $tipos, [], ['class' => 'form-control', 'required']) !!}
+    
+        
+    
+        @error('tipo_id')
+            <span class="text-danger">{{$message}}</span>
+        @enderror
+    
+    </div>
+</div>
+    <div class="col-lg-6 form-group">
         {!! Form::label('descripcion', 'Descripcion', ['class' => 'is-required']) !!}
         {!! Form::textarea('descripcion', null, ['class' => 'form-control', 'placeholder' => 'Introduzca la descripción de la instancia', 'autocomplete' => 'off', 'required']) !!}
     
@@ -28,34 +41,30 @@
     
     </div>
 </div>
-
 <div class="row">
+    <p>{!! Form::label('localizacion', 'Localizacion', ['class' => 'is-required']) !!}</p>
+    {!! Form::checkbox('localizacion',  null, ['required']) !!}
+</div>
 
-    <div class="col-lg-6 form-group">
-        {!! Form::label('tipo_id', 'Tipo', ['class' => 'is-required']) !!}
-        {!! Form::select('role', $tipos, [], ['class' => 'form-control', 'required']) !!}
-
-        
-
-        @error('tipo_id')
-            <span class="text-danger">{{$message}}</span>
-        @enderror
-
-    </div>
-
+@if ('localizacion'==1)
+    
+<div class="row">
+    
+    
     <div class="col-lg-6 form-group">
         {!! Form::label('capacidad_id', 'Capacidad', ['class' => 'is-required']) !!}
         {!! Form::number('capacidad_id', null, ['class' => 'form-control', 'placeholder' => 'Introduzca la capacidad', 'autocomplete' => 'off', 'required' ]) !!}
-
         
-
+        
+        
         @error('capacidad_id')
-            <span class="text-danger">{{$message}}</span>
+        <span class="text-danger">{{$message}}</span>
         @enderror
- 
+        
     </div>
 </div>
 
+@endif
 </div>
 
 
