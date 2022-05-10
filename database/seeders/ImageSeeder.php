5b4350a249b1c4933c0d9b403c4172c7e3952763
danielmartin2 <danielmartinteruel@gmail.com>
2022-05-10 15:04:16 +0200

    seeder imagen

--- database/seeders/ImageSeeder.php
+++ database/seeders/ImageSeeder.php
@@ -15,19 +15,34 @@ class ImageSeeder extends Seeder
     public function run()
     {
         Image::create([
-            'url'=>'http://127.0.0.1:8000/images/post/person_1.jpg',
+            'url'=>'http://127.0.0.1:8000/images/Finca Santa Ana/finca.jpg',
             'principal'=>1,
             'sector_id'=>1, 
         ]);
         Image::create([
-            'url'=>'http://127.0.0.1:8000/images/post/person_2.jpg',
-            'principal'=>0,
-            'sector_id'=>1, 
+            'url'=>'http://127.0.0.1:8000/images/Navarrete del río/navarrete2.JPG',
+            'principal'=>1,
+            'sector_id'=>2, 
         ]);
         Image::create([
-            'url'=>'http://127.0.0.1:8000/images/post/bg_3.jpg',
+            'url'=>'http://127.0.0.1:8000/images/Lago Azul/lagoAzul.jpg',
             'principal'=>1,
-            'sector_id'=>1, 
+            'sector_id'=>4, 
+        ]);
+        Image::create([
+            'url'=>'http://127.0.0.1:8000/images/Escaleruela/mesa-central.jpg',
+            'principal'=>1,
+            'sector_id'=>3, 
+        ]);
+        Image::create([
+            'url'=>'http://127.0.0.1:8000/images/El Mercao/mercaocentral.jpg',
+            'principal'=>1,
+            'sector_id'=>5, 
+        ]);
+        Image::create([
+            'url'=>'http://127.0.0.1:8000/images/Bonet/bonet6.jpg',
+            'principal'=>1,
+            'sector_id'=>6, 
         ]);
     }
 }
