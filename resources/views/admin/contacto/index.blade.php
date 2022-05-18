0535458bac10b78f5e6742213aa383773d989eab
cesargorrizm <cgorrim@iesch.org>
2022-05-18 11:29:58 +0200

    ds

--- resources/views/admin/contacto/index.blade.php
+++ resources/views/admin/contacto/index.blade.php
@@ -136,7 +136,7 @@
         $(function () {
             $('#contactos').DataTable({
                 columnDefs: [ {
-        targets: [2,3,4,5],
+        targets: [1,2,3,4,5],
         render: function ( data, type, row ) {
             return data.substr( 0, 10 )+'…';
         }
